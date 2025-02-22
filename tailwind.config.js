0ceb7d0ca568c01d2edeaf1fe77525da3dfb8864
teguhpermadi <tguh.permadi@gmail.com>
2025-02-22 19:02:19 +0700

    feat(dependencies): add guava/filament-modal-relation-managers package and update Tailwind config for blade file inclusion

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './vendor/guava/filament-modal-relation-managers/resources/**/*.blade.php',
     ],
     theme: {
         extend: {
