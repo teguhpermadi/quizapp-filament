953ef3f0bd6c0b93057e436606b4018d9eaa3968
teguhpermadi <tguh.permadi@gmail.com>
2025-03-23 08:49:38 +0700

    feat(database): rename migration table from 'table_paragraph_question' to 'paragraph_question' and update query joins for exams and questions

--- database/migrations/2025_03_12_043932_create_table_paragraph_question.php
+++ database/migrations/2025_03_12_043932_create_table_paragraph_question.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_paragraph_question');
+        Schema::dropIfExists('paragraph_question');
     }
 };
