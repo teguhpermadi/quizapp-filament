726b90fe4934739eed572b6c27c08f8b28ec598e
teguhpermadi <tguh.permadi@gmail.com>
2025-03-28 15:48:21 +0700

    style(paragraph): update background color of ParagraphQuestion component for improved visibility

--- resources/views/livewire/paragraph-question.blade.php
+++ resources/views/livewire/paragraph-question.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="bg-white border border-gray-300 rounded-lg p-4 shadow-md w-full">
+    <div class="bg-amber-100 border border-amber-300 rounded-lg p-4 shadow-md w-full">
         <div class="p-2">
             <p>{{ Str::limit($paragraph->paragraph, 300) }}</p>
         </div>
