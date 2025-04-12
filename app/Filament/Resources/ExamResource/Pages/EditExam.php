4f897c3d65469a3d795c66fc46ef992a9a01f359
teguhpermadi <tguh.permadi@gmail.com>
2025-04-12 19:02:19 +0700

    feat(exam): implement redirect functionality after editing an exam

--- app/Filament/Resources/ExamResource/Pages/EditExam.php
+++ app/Filament/Resources/ExamResource/Pages/EditExam.php
@@ -18,4 +18,9 @@ class EditExam extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
 }
